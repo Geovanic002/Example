103d081a76bb3bc4a9e0e35277a8b989b0d21970
unknown <Geovanic002>
2021-04-29 08:29:28 -0500

    29-04-2021  prueba

---
+++ Prueba.py
@@ -0,0 +1,20 @@
+x="hola mundo"
+y=4.5
+n=53
+u=True
+
+print(x)
+print(type(x))
+
+print(y)
+print(type(y))
+
+print(n)
+print(type(n))
+
+print(u)
+print(type(u))
+
+entrada=input("Ingrese una palabra \n")
+
+print("Hola "+entrada)
---
+++ random.py
@@ -0,0 +1,14 @@
+"""from random import randint
+
+import random   
+import string
+
+lista1=[]
+
+for 1 in range (10)
+"""
+
+X="Hola Mundo"
+
+print(X.upper())
+print(X.lower())
